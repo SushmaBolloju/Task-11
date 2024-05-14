ef0408158da5f17cadf9c6d81c9c9c528d3c8e8d
SushmaBolloju <160645457+SushmaBolloju@users.noreply.github.com>
2024-05-14 21:05:11 +0530

    Added required comments.

--- actions.py
+++ actions.py
@@ -13,6 +13,7 @@ chrome_options=Options()
 chrome_options.add_experimental_option("detach", True)
 driver = webdriver.Chrome(options=chrome_options)
 time.sleep(3)
+# open the window using given url
 driver.get("https://jqueryui.com/droppable/")
 driver.maximize_window()
 time.sleep(3)
@@ -24,16 +25,18 @@ driver.switch_to.frame(driver.find_element(By.XPATH,'//*[@id="content"]/iframe')
 draggable_element = driver.find_element(By.ID,'draggable')
 print(draggable_element.text)
 
+# Find the droppable element
 droppable_element = driver.find_element(By.ID, 'droppable')
 
+# perform drag and drop action
 actions = ActionChains(driver)
 actions.drag_and_drop(draggable_element, droppable_element).perform()
 
+# check if drag and drop action is successful
 if "Dropped!" in droppable_element.text:
     print("Drag and drop successful!")
 else:
     print("Drag and drop failed.")
 
 time.sleep(5)
-
 driver.quit()
