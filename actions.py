c617a5379cf33537ec68f6afae0a540ee43af343
SushmaBolloju <160645457+SushmaBolloju@users.noreply.github.com>
2024-05-14 20:59:36 +0530

    Selenium Code for dragging an element and dropping into another.

---
+++ actions.py
@@ -0,0 +1,39 @@
+
+
+import os
+from selenium import webdriver
+from selenium.webdriver import ActionChains
+from selenium.webdriver.chrome.options import Options
+import time
+from selenium.webdriver.common.by import By
+
+paths = r"C:\Users\chand\Downloads\chromedriver-win32\chromedriver-win32\chromedriver.exe"
+os.environ["PATH"] += os.pathsep + os.path.dirname(paths)
+chrome_options=Options()
+chrome_options.add_experimental_option("detach", True)
+driver = webdriver.Chrome(options=chrome_options)
+time.sleep(3)
+driver.get("https://jqueryui.com/droppable/")
+driver.maximize_window()
+time.sleep(3)
+
+# Switch to the frame containing the draggable elements
+driver.switch_to.frame(driver.find_element(By.XPATH,'//*[@id="content"]/iframe'))
+
+# Find the draggable element
+draggable_element = driver.find_element(By.ID,'draggable')
+print(draggable_element.text)
+
+droppable_element = driver.find_element(By.ID, 'droppable')
+
+actions = ActionChains(driver)
+actions.drag_and_drop(draggable_element, droppable_element).perform()
+
+if "Dropped!" in droppable_element.text:
+    print("Drag and drop successful!")
+else:
+    print("Drag and drop failed.")
+
+time.sleep(5)
+
+driver.quit()
